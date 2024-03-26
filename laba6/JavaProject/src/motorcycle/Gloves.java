34f9361a098e770651627a5622271fe7aa701322
Kalchevskiy_Daniil <95997338+Kalchevskiy-Daniil-Andreevich@users.noreply.github.com>
2024-03-26 19:02:35 +0300

    Delete laba6/JavaProject directory

--- laba6/JavaProject/src/motorcycle/Gloves.java
+++
@@ -1,7 +0,0 @@
-package motorcycle;
-
-public class Gloves extends Ammunition {
-    public Gloves(String name, double price, double weight) {
-        super(name, price, weight);
-    }
-}
--- laba6/JavaProject/src/motorcycle/Helmet.java
+++
@@ -1,7 +0,0 @@
-package motorcycle;
-
-public class Helmet extends Ammunition {
-    public Helmet(String name, double price, double weight) {
-        super(name, price, weight);
-    }
-}
