f8dc7ee07f029a0123a20138bbde6c3117ea7f3d
Kalchevskiy_Daniil <95997338+Kalchevskiy-Daniil-Andreevich@users.noreply.github.com>
2024-03-26 19:02:47 +0300

    Delete laba7 directory

--- laba7/firstJob.js
+++
@@ -1,9 +0,0 @@
-function firstJob() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Hello World');
-        }, 2000);
-    });
-}
-
-module.exports = firstJob;
