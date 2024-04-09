1f70905a53856d0ef84c3edb6c5296441238e6d5
yogick <bankahl36@gmail.com>
2024-04-09 11:47:15 +0300

    added the laba9 folder

---
+++ laba9/automationTestCase.js
@@ -0,0 +1,28 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+async function addToCartTest() {
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+        await driver.get('https://www.elmarket.by/');
+
+        let searchInput = await driver.findElement(By.id('search-field'));
+        await searchInput.sendKeys('Braun MQ 3135 WH Sauce', Key.RETURN);
+
+        await driver.wait(until.elementLocated(By.className('catalog list search-catalog-block')), 5000);
+
+        await sleep(3000);
+
+        console.log('Товар успешно найден!');
+    } catch (error) {
+        console.error('Произошла ошибка:', error);
+    } finally {
+        await driver.quit();
+    }
+}
+
+addToCartTest();
